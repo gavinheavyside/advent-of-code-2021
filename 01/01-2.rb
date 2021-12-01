4f2519f4a8dbf16a3ef71237408da9b2ba700cb2
Gavin Heavyside <gavin@heavyside.co.uk>
2021-12-01 17:44:38 +0000

    Formatting

--- 01/01-2.rb
+++ 01/01-2.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-input = $stdin.readlines.map{|row| Integer(row.chomp)}
+input = $stdin.readlines.map{ |row| Integer(row.chomp) }
 
 threewise = input.each_cons(3).map { |a,b,c| a+b+c }
 
-puts threewise.each_cons(2).select { |a,b| b > a }.size
+puts threewise.each_cons(2).filter { |a,b| b > a }.size
