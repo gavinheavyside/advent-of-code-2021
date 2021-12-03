d99d3749f9fa5adf0d0165f1b41b50bf35a4551f
Gavin Heavyside <gavin@heavyside.co.uk>
2021-12-03 15:48:08 +0000

    Clean up

--- 03/03-2.rb
+++ 03/03-2.rb
@@ -3,25 +3,23 @@
 def partition(working_set, column_number, type)
   return working_set if working_set.size == 1
 
-  group1, group0 = working_set.partition { |row| row[column_number] == '1' }
+  groups = working_set.partition { |row| row[column_number] == '1' }
 
   if type == :o2
-    group0.size > group1.size ? group0 : group1
+    partition(groups.max_by(&:size), column_number + 1, type)
   else
-    group1.size < group0.size ? group1 : group0
+    partition(groups.reverse.min_by(&:size), column_number + 1, type)
   end
 end
 
 def rating(working_set, type)
-  (0..working_set.first.size).each do |i|
-    working_set = partition(working_set, i, type)
-  end
-  working_set.first.join.to_i(2)
+  rating_set = partition(working_set.dup, 0, type)
+  rating_set.first.join.to_i(2)
 end
 
 working_set = $stdin.readlines.map{ |row| row.chomp.chars }
 
-o2_rating = rating(working_set.dup, :o2)
-co2_rating = rating(working_set.dup, :co2)
+o2_rating = rating(working_set, :o2)
+co2_rating = rating(working_set, :co2)
 
 puts o2_rating * co2_rating
