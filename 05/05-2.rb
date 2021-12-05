d07119bb1aa0d03ba712eeee4c9a8d64ad111198
Gavin Heavyside <gavin@heavyside.co.uk>
2021-12-05 14:38:26 +0000

    Part 2

--- 05/05-2.rb
+++ 05/05-2.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 
 def interpolate(a, b)
-  (a > b) ? (b..a).to_a : (a..b).to_a
+  (a > b) ? (b..a).to_a.reverse : (a..b).to_a
 end
 
 rows = $stdin.readlines.map{ |row| row.chomp.split }
 
 coord_pairs = rows.map do |row|
   [row.first, row.last].map { |pair| pair.split(',').map(&:to_i) }
-end.select do |(x1,y1), (x2,y2)|
-  x1 == x2 || y1 == y2
 end
-
-coord_pairs.size 
 
 vented = Hash.new { |k,v| k[v] = 0}
 
 coord_pairs.each do |(x1,y1), (x2,y2)|
-  interpolate(x1, x2).each do |x|
-    interpolate(y1, y2).each do |y|
-      vented[[x,y]] += 1
-    end
+  x_coords = interpolate(x1, x2)
+  y_coords = interpolate(y1, y2)
+
+  x_coords *= y_coords.size if x_coords.size == 1
+  y_coords *= x_coords.size if y_coords.size == 1
+
+  x_coords.zip(y_coords).each do |(x,y)|
+    vented[[x,y]] += 1
   end
 end
 
