4091b2395a29b1701d7ea5af45a3c4350c3e5be1
Gavin Heavyside <gavin@heavyside.co.uk>
2021-12-07 19:59:06 +0000

    Day 06

---
+++ 06/06-1.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+timers = Hash.new{ |k,v| k[v] = 0 }
+
+$stdin.read.chomp.split(',').each{ |t| timers[t.to_i] += 1 }
+
+#puts timers.inspect
+
+80.times do |j|
+  0.upto(8) do |i|
+    timers[i-1] = timers[i]
+  end
+  timers[6] += timers[-1]
+  timers[8]  = timers[-1]
+
+#  puts "tick #{j}"
+#  puts timers.inspect
+end
+
+puts timers.select{ |k,v| !k.negative? }.map{ |k,v| v}.reduce(:+)
