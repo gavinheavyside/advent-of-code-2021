60aef0af95b62b7049ca1ede883779cb9b6d3fc1
Gavin Heavyside <gavin@heavyside.co.uk>
2021-12-12 19:26:45 +0000

    Day 11 part 1

---
+++ 11/11-1.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+def tick_neighbourhood(grid, x, y)
+  min_x = [0, x-1].max
+  min_y = [0, y-1].max
+  max_x = [grid.first.size-1, x+1].min
+  max_y = [grid.size-1, y+1].min
+
+  (min_x..max_x).each do |x|
+    (min_y..max_y).each do |y|
+      grid[y][x][:value] += 1
+      if grid[y][x][:value] > 9 && !grid[y][x][:flashed]
+        grid[y][x][:flashed] = true
+        tick_neighbourhood(grid, x, y)
+      end
+    end
+  end
+end
+
+def tick(grid)
+  grid.each_with_index do |row, y|
+    row.each_with_index do |cell, x|
+      grid[y][x][:value] += 1
+      if grid[y][x][:value] > 9 && !grid[y][x][:flashed]
+        grid[y][x][:flashed] = true
+        tick_neighbourhood(grid, x, y)
+      end
+    end
+  end
+
+  flashes = 0
+  grid.each_with_index do |row, y|
+    row.each_with_index do |cell, x|
+      if cell[:flashed]
+        flashes += 1
+        grid[y][x][:value] = 0
+        grid[y][x][:flashed] = false
+      end
+    end
+  end
+
+  flashes
+end
+
+grid = $stdin.readlines.map do |r|
+  r.chomp.chars.map{ |c| { flashed: false, value: c.to_i } }
+end
+
+total_flashes = 0
+
+100.times do
+#  puts grid.map{|r| r.map{|c| c[:value]}.join }
+#  puts total_flashes
+#  puts
+  total_flashes += tick(grid)
+end
+
+#puts grid.map{|r| r.map{|c| c[:value]}.join }
+#puts
+
+puts total_flashes
